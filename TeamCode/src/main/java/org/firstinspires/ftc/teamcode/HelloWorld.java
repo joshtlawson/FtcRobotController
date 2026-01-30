5ae4304183025ac1e466404f3dde935bdfdc75e2
joshtlawson <146592606+joshtlawson@users.noreply.github.com>
2026-01-29 19:14:29 -0600

    Initial Test File
    We'll throw this away later

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@Disabled
+@Autonomous
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello ","Big Beefcake Dad monster!");
+    }
+    @Override
+    public void loop(){
+
+    }
+
+
+}
